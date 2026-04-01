8c3d752f9b9ff34cc537580c1a713a8261f73206
OlenaYermak <145419464+OlenaYermak@users.noreply.github.com>
2026-04-01 17:07:50 +0300

    lesson3

---
+++ lessons/lesson-03/script.js
@@ -0,0 +1,147 @@
+// =************************************************************
+
+console.log("Example 1 — Сума елементів масиву");
+
+const numbers = [1, 2, 3, 4, 5];
+
+let total = 0;
+
+for (const num of numbers) {
+    total += num;
+}
+
+console.log(total);
+
+// =************************************************************
+
+console.log("Example 2 — Парні числа з масиву");
+
+const numbers2 = [1, 223, 30, 4, 5, 6, 8, 22, 15, 90, 60, 53];
+
+let newArr = [];
+
+for (let i = 0; i < numbers2.length; i++){
+    if (numbers2[i] % 2 === 0) {
+        newArr.push(numbers2[i]);
+    }
+}
+
+console.log(newArr);
+
+// =************************************************************
+
+// console.log("Example 3 — Найбільше число");
+
+// const numbers3 = [5, 12, 3, 20, 8];
+
+// =************************************************************
+
+console.log("Example 4 — Реверс масиву");
+
+const numbers4 = [1, 2, 3];
+
+let numb = [];
+
+for (let i = numbers4.length - 1; i >= 0; i--){
+    numb.push(numbers4[i]);
+}
+
+console.log(numb);
+
+// =************************************************************
+
+console.log("Example 5 — Підрахунок елементів");
+
+const numbers5 = [5, 15, 8, 20, 3];
+let array = [];
+
+for (const num of numbers5) {
+    if (num > 10) {
+        array.push(num); 
+    }
+}
+console.log(array.length);
+
+// =************************************************************
+
+console.log("Example 6 — includes");
+
+const fruits = ["apple", "banana", "orange"];
+
+fruits.includes("banana") ? console.log("Found") : console.log("Not found");
+
+// =************************************************************
+
+console.log("Example 7 — join");
+
+const words = ["Hello", "world"];
+
+let newWords = words.join(" ");
+console.log(newWords);
+
+// =************************************************************
+
+console.log("Example 8 — split");
+
+const text = "a,b,c";
+
+let newText = text.split(",");
+console.log(newText);
+
+// =************************************************************
+
+console.log("Example 9 — Function Declaration");
+ 
+console.log(sum(5, 10)); //можна викликати до оголошення
+
+function sum(a, b) { 
+    return a + b;
+}
+
+console.log(sum(55, 10));
+
+// =************************************************************
+
+console.log("Example 10 — Function Expression");
+
+// !!!! console.log(isEven(2)) - не можна викликати до ініціалізації
+
+const isEven = function (num) { 
+    let msg = "";
+    (num % 2 === 0) ? msg = true : msg = false;
+    return msg;
+};
+
+console.log(isEven(2));
+
+// =************************************************************
+
+console.log("Example 11 — Параметри за замовчуванням");
+
+function greet(name = "Guest") { 
+
+    console.log(`Hello, ${name}`);
+}
+
+console.log(greet());
+console.log(greet("Ihor"));
+
+// =************************************************************
+
+console.log("Example 12 — arguments (псевдомасив)");
+
+function sumAll() {
+    let arg = Array.from(arguments);
+    let total = null;
+
+    for (const a of arg) {
+        total += a;
+    }
+    console.log(total);
+}
+
+sumAll(1, 2, 3, 4);
+ 
+// =************************************************************
+
+console.log("Example 13 — ");
