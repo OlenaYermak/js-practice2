332ab820caf6bf25f47ed516df40cfe707e3d4f6
OlenaYermak <145419464+OlenaYermak@users.noreply.github.com>
2026-05-16 13:14:52 +0300

    lesson6.

--- lessons/lesson-06/script.js
+++ lessons/lesson-06/script.js
@@ -78,11 +78,14 @@ audi.showModel();
 console.log("Example 7 — Основи OOP");
 
 class User2 {
-    constructor(name, age) {
-        this.name = name;
-        this.age = age;
+    constructor() {
+        this.products = [];
     }
- }
+}
+ 
+addProduct(product){
+    this.products.push(product);
+}
 
 const user7 = new User2("Olena", 25);
 
@@ -96,4 +99,130 @@ class User3 {
     constructor(name) {
         this.name = name;
     }
+
+    getName() {
+        return this.name;
+    }
 }
+
+
+// =************************************************************
+
+console.log("Example 9 — Наслідування");
+
+class User9 {
+    constructor(name) {
+        this.name = name;
+    }
+}
+
+class Admin extends User9{
+    role;
+}
+
+console.log(Admin);
+
+// =************************************************************
+
+console.log("Example 10 — super()");
+
+class User10 {
+    constructor(name) {
+        this.name = name;
+    }
+}
+
+class Admin2 extends User10{
+    constructor(name) {
+        
+        super(name)
+    }
+}
+
+const admin2 = new Admin2({
+    name: "Olga"
+})
+
+console.log(admin2)
+
+
+// =************************************************************
+
+console.log("Example 11 — Приватна властивість");
+
+class BankAccount { 
+    #balanse
+    constructor(balanse) {
+        this.#balanse = balanse;
+    }
+
+    get balanse() {
+        return this.#balanse;
+    }
+}
+
+
+// =************************************************************
+
+console.log("Example 12 — Getter і Setter");
+
+class User12 {
+    #email;
+    constructor(name, email) {
+        this.name = name;
+        this.#email = email;
+    }
+
+    get email() {
+        return this.#email
+    }
+
+    set email(newEmail) {
+        this.#email = newEmail;
+    }
+}
+
+// =************************************************************
+
+console.log("Example 13 — Статичний метод");
+
+class MathHelper {
+    static square(num) {
+        return num * num;
+    }
+ }
+
+console.log(MathHelper.square(4));
+
+// =************************************************************
+
+console.log("Example 14 — Реальний кейс (кошик)");
+
+class Cart {
+    constructor(params) {
+        this.name = params.name;
+        this.price = params.price;
+    };
+
+    addProduct(product) {
+       let productList = productList.push(product);
+    }
+
+    // getTotalPrice() {
+    //   let totalPrice =
+    // }
+}
+
+const cart = new Cart();
+
+
+//=************************************************************
+
+console.log("Example 15 — Міні-челендж (this)");
+
+const user15 = {
+    name: "Olena",
+    showName() { console.log(this.name); }
+};
+console.log(user15.showName());
+
